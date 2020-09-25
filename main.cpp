04379893cb9824d0c6536fed0a76cf8273606d3e
Swung0x48 <swung0x48@outlook.com>
2020-09-25 12:16:29 +0800

    A commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Hello", "World!"};
 
     for (const string& word : msg)
     {
